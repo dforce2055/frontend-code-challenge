396f5d2f5f752e11d37e95c3a79f8992620832a2
Diego Pérez <dperez2055@gmail.com>
2024-07-14 02:29:18 -0300

    WIP ActivityDetails

---
+++ src/views/ActivityDetails.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { defineProps, onBeforeMount, ref } from 'vue'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+
+const loading = ref<boolean>(true)
+
+
+onBeforeMount(async () => {
+  // TODO: Call API with id
+  console.log('Call API with id:', props.id)
+})
+</script>
+<template>
+  <section>
+    <h1>Activity detail</h1>
+    <h2>Activity: {{ props.id }}</h2>
+  </section>
+</template>
