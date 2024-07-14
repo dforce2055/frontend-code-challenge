fa7ea0c65a57e54ca18f0559ad493cb5859b2169
Diego Pérez <dperez2055@gmail.com>
2024-07-14 02:27:27 -0300

    Adding routing animation transitions

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,20 @@ onBeforeMount(async () => {
 
 <template>
   <Notification />
-  <RouterView />
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
