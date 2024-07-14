4c12bafdaf2e676fb2bd424b0bc11473230e4baf
Diego Pérez <dperez2055@gmail.com>
2024-07-14 02:29:52 -0300

    Adding navigation

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+<template>
+  <main>
+    <header>
+      <div class="wrapper h-24 mt-4">
+        <nav class="flex items-center justify-between w-6/12 mx-auto">
+          <RouterLink to="/">
+            <button type="button"
+              class="rounded bg-primary px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Home</button>
+          </RouterLink>
+          <RouterLink to="/actividades"><button type="button"
+              class="rounded bg-red-100 px-2 py-1 text-xs font-semibold text-red-900 shadow-sm hover:bg-primary hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Actividades</button>
+          </RouterLink>
+        </nav>
+      </div>
+    </header>
+    <h1 class="text-red-500 text-8xl mt-12">Frontend Code Challenge</h1>
+  </main>
+</template>
