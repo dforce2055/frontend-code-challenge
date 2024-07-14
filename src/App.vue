3536e6a0f008d7921d60924420eafe9e54bfb73b
Diego Pérez <dperez2055@gmail.com>
2024-07-13 23:05:07 -0300

    Adding store to handle notifications

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Notification from './components/Notification/Notification.vue'
+
 </script>
 
 <template>
+  <Notification />
   <RouterView />
 </template>
